055610e574524066cacee0b60776012883cac547
EduanG3 <gdan68338@gmail.com>
2024-05-29 16:42:05 -0600

    laboratorio 1 RStudio en Clase

---
+++ Laboratorios/Laboratorio 1.R
@@ -0,0 +1,24 @@
+# Dan Eduardo González García 
+# Matrícula: 1977438
+# 24/04/2024
+
+# Laboratorio 1
+ 
+# Gastos totales
+
+300 + 240 + 1527 + 1500 + 1833
+
+# Crear objetos con valor 
+
+celular <- 300 
+
+# Operaciones con objetos 
+
+celular + celular # suma
+celular * 3 # multiplicación 
+celular /2 # división 
+
+
+
+
+
